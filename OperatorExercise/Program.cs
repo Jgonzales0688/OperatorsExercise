bf2f01e540cbef31b851562581eb000d4f390b47
Jessica Gonzales <jgonzales0688@gmail.com>
2022-11-28 09:52:19 -0600

    operators exercise

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace OperatorsInClass
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Exercise 1
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            
+            Console.WriteLine($" {a} / {b} is {quotient} remainder {remainder}");
+
+            //Exercise 2
+            Console.WriteLine("Please enter the radius of your circle");
+           
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle with the radius of {radius} is {AreaOfCircle(radius)}");
+            
+        }
+        public static double AreaOfCircle(double radius) 
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+
+        }
+    }
+}
+
+
